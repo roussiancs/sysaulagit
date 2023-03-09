3082011843620f907b1f8ca46e27e6e2deaa371c
Roussian Gaioso <roussian.comp@gmail.com>
2023-03-09 19:00:47 -0300

    Criação do Projeto sysaulagit

---
+++ src/main/java/br/com/aulagit/sysaulagit/SysaulagitApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulagit.sysaulagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysaulagitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysaulagitApplication.class, args);
+	}
+
+}
