776a3e97a7445673004de90d8de73fe6b70d107f
Roussian Gaioso <roussian.comp@gmail.com>
2023-03-09 19:09:32 -0300

    "Criação da Classe Cliente"

---
+++ src/main/java/br/com/aulagit/sysaulagit/domain/Cliente.java
@@ -0,0 +1,28 @@
+package br.com.aulagit.sysaulagit.domain;
+
+public class Cliente {
+    private String nome;
+    private String idade;
+    private String endereco;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getIdade() {
+        return idade;
+    }
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    
+}
